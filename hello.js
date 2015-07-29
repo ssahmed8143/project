7e3d0b6324ea873cf6469e4235cad3300d66f987
Sulthan <ssahmed8143@example.com>
2015-07-29 15:02:39 +0100

    some changes to show javascript

--- hello.js
+++ hello.js
@@ -1,3 +1,50 @@
 /**
  * Created by Sulthan on 19/05/15.
  */
+
+//document.write("hello " + "<br />");
+console.log("test");
+var ThisIsV; //undefined CameCase
+ThisIsV= "something";
+alert(ThisIsV);
+var x=10;
+var isBool= false;
+var foo = parseInt("34s57", 10) //34
+
+function givePie(){
+    return 3.14;
+}
+
+function multiplyByTwo(a){
+    return a*2;
+}
+
+var product = function (a, b){
+    return a * b;
+}
+
+
+var doSomething = function(p1,p2, fn){
+    p1= p1 + 1;
+    return fn(p1, p2);
+}
+
+
+function sum(p1, p2){
+    return p1 + p2;
+}
+
+var foo = doSomething(2, 2, sum);
+
+var bar = doSomething(2, 2, function (p1, p2) {
+    return p1 + p2;
+});
+
+
+
+document.write(givePie() + "<br />");
+document.write(multiplyByTwo(2) + "<br />");
+document.write(product(1, 3) + "<br />");
+document.write(foo + "<br />");
+document.write(bar + "<br />");
+
